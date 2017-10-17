67f87a5f9f96f0d6b3c4e837a7f2145c313b6ad2
Alec Sands <sandsalecv@gmail.com>
2017-10-17 14:36:20 -0500

    removed code comments from previous steps

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,25 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// Classes (ES6) are in fact "special functions", and just as you can define function
-// expressions and function declarations, the class syntax has two components:
-// class expressions and class declarations.
 
-// The extends keyword (ES6) is used in class declarations or class expressions to
-// create a class which is a child of another class.
-
-// React has already created a class called React.Component and Square is a child
-// of that already existing class.
-
-// This is an example of a class declaration.
 class Square extends React.Component {
-  // The constructor method (ES6) is a special method for creating and initializing
-  // an object created within a class.
   constructor() {
-    // The super keyword (ES6) is used to call functions on an object's parent.
-
-    // When super doesn't have a method chained to it then it is calling the
-    // constructor from the parent class.
     super();
     this.state = {
       value: null,
@@ -85,19 +69,7 @@ class Game extends React.Component {
 
 // ========================================
 
-// render() ---- REACT
-
-// Render a React element into the DOM in the supplied container and return a
-// reference to the component (or returns null for stateless components).
-//
-// If the React element was previously rendered into container, this will perform
-// an update on it and only mutate the DOM as necessary to reflect the latest
-// React element.
-
 ReactDOM.render(
-  // This is the element that will be rendered to the DOM.  All other Classes
-  // are embedded within it so they will be rendered as well.
   <Game />,
-  // This is the container that all rendered elements will be placed into.
   document.getElementById('root')
 );
